3b91775b1ac8fb4a4010e4b93875fefa4cbeaada
deezy <deezy@deezys-MacBook-Pro.local>
2020-03-01 14:43:21 -0800

    update install script

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker > signature-checker
 curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker.asc > signature-checker.asc
 gpg --recv-keys 4BC04EAFEC6BCD82EC18677A6538139AE7AF3E17
-gpg --verify signature-checker.asc signature-checker > /dev/null
+gpg --verify signature-checker.asc signature-checker > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   chmod a+x signature-checker
   tput setaf 2
-  printf "\n\nSuccessfully verified and installed to your Desktop. Double-click the signature-checker file run!\n\n"
+  printf "\n\nSuccessfully verified and installed \xE2\x9C\x94\n\nDouble-click the file on your Desktop called signature-checker to run.\n\n"
   tput sgr0
 else
   tput setaf 1
