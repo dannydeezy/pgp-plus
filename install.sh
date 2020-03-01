04bdb5a449af7c0fe2ab5c25279efaa3dbe9dcad
deezy <deezy@deezys-MacBook-Pro.local>
2020-03-01 14:40:11 -0800

    suppress output of install verify

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker > signature-checker
 curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker.asc > signature-checker.asc
 gpg --recv-keys 4BC04EAFEC6BCD82EC18677A6538139AE7AF3E17
-gpg --verify signature-checker.asc signature-checker
+gpg --verify signature-checker.asc signature-checker > /dev/null
 if [ $? -eq 0 ]; then
   chmod a+x signature-checker
   tput setaf 2
