79974d37d41397584fdfe39d2cacc68e9041222c
deezy <deezy@deezys-MacBook-Pro.local>
2020-03-01 14:33:42 -0800

    Fix signature

--- install.sh
+++ install.sh
@@ -3,5 +3,11 @@ curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-chec
 curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker.asc > signature-checker.asc
 gpg --recv-keys 4BC04EAFEC6BCD82EC18677A6538139AE7AF3E17
 gpg --verify signature-checker.asc signature-checker
-chmod a+x signature-checker
+if [ $? -eq 0]; then
+  chmod a+x signature-checker
+  printf "\n\nSuccessfully verified and installed\n\n"
+else
+  printf "\n\nSomething went wrong\n\n"
+  exit 1
+fi
 rm signature-checker.asc
