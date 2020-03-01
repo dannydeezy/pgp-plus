3634f7a5deadaf3440cd0ff911dde6c5c39f6e7d
deezy <deezy@deezys-MacBook-Pro.local>
2020-03-01 14:37:56 -0800

    Update words in install script

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ gpg --recv-keys 4BC04EAFEC6BCD82EC18677A6538139AE7AF3E17
 gpg --verify signature-checker.asc signature-checker
 if [ $? -eq 0 ]; then
   chmod a+x signature-checker
-  printf "\n\nSuccessfully verified and installed\n\n"
+  tput setaf 2
+  printf "\n\nSuccessfully verified and installed to your Desktop. Double-click the signature-checker file run!\n\n"
+  tput sgr0
 else
+  tput setaf 1
   printf "\n\nSomething went wrong\n\n"
+  tput sgr0
   exit 1
 fi
 rm signature-checker.asc
