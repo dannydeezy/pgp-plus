590aadf829c64bd4c81eac90dc9cef06a39803a8
Danny Diekroeger <dannydiek@gmail.com>
2020-03-01 16:04:39 -0800

    Fix URLs in install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker > signature-checker
-curl https://raw.githubusercontent.com/dannydeezy/easy-pgp/master/signature-checker.asc > signature-checker.asc
+curl https://raw.githubusercontent.com/dannydeezy/pgp-plus/master/signature-checker > signature-checker
+curl https://raw.githubusercontent.com/dannydeezy/pgp-plus/master/signature-checker.asc > signature-checker.asc
 gpg --recv-keys 4BC04EAFEC6BCD82EC18677A6538139AE7AF3E17
 gpg --verify signature-checker.asc signature-checker > /dev/null 2>&1
 if [ $? -eq 0 ]; then
